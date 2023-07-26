08b8e357db0fc4ff94d59b8a337af3898c231b60
Vladimir <vladimircuriel@outlook.com>
2023-07-26 00:16:50 -0400

    cambios videos 3

--- index.js
+++ index.js
@@ -1,70 +1,67 @@
 const url = "https://jsonplaceholder.typicode.com";
 
-const getUser = async (id) =>
-{
-  const res = await fetch(`${url}/users?id=${id}`);
-  const user = (await res.json())[0];
+const getUser = async (id) => {
+	const res = await fetch(`${url}/users?id=${id}`);
+	const user = (await res.json())[0];
 
-  return user;
-}
+	return user;
+};
 
-const getPosts = async (user) =>
-{
-  const res = await fetch(`${url}/posts?userId=${user.id}&_limit=3`)
-  const posts = await res.json();
+const getPosts = async (user) => {
+	const res = await fetch(`${url}/posts?userId=${user.id}&_limit=3`);
+	const posts = await res.json();
 
-  return posts;
-}
+	return posts;
+};
 
-const getCommentsForEachPost = async (posts) =>
-{
-  const res = await Promise.all(posts.map(post =>
-    fetch(`${url}/comments?postId=${post.id}&_limit=3`)
-  ))
-  const postComments = await Promise.all(res.map(r => r.json()));
+const getCommentsForEachPost = async (posts) => {
+	const res = await Promise.all(
+		posts.map((post) => fetch(`${url}/comments?postId=${post.id}&_limit=3`))
+	);
+	const postComments = await Promise.all(res.map((r) => r.json()));
 
-  postComments.forEach((comments, i) => posts[i].comments = comments);
-}
-const renderHtml = (user, posts) =>
-{
-   const content = document.getElementById('content');
-   content.innerHTML += `<h3>Posts del usuario ${user.email}</h3>`;
+	postComments.forEach((comments, i) => (posts[i].comments = comments));
+};
+const renderHtml = (user, posts) => {
+	const content = document.getElementById("content");
+	content.innerHTML += `<h3>Posts del usuario ${user.email}</h3>`;
 
-   posts.forEach(post => {
-     content.innerHTML += `
+	posts.forEach((post) => {
+		content.innerHTML += `
      <div class="post">
        <h4>${post.title}</h4>
        <p>${post.body}</p>
        <br>
-       ${post.comments.map(c => `<p><span>${c.email}:</span>${c.body}</p>`).join('')}
+       ${post.comments
+				.map((c) => `<p><span>${c.email}:</span>${c.body}</p>`)
+				.join("")}
      </div>
      `;
-   })
-}
+	});
+};
 
+const getBlogContent = async () => {
+	try {
+		const user = await getUser(1);
+		const posts = await getPosts(user);
+		await getCommentsForEachPost(posts);
 
-const getBlogContent = async () =>
-{
-  try {
-    const user = await getUser(1);
-    const posts = await getPosts(user);
-    await getCommentsForEachPost(posts);
-
-    renderHtml(user, posts);
-  }
-  catch (err)
-  {
-    console.log(err);
-  }
-}
+		renderHtml(user, posts);
+	} catch (err) {
+		console.log(err);
+	}
+};
 
 getBlogContent();
 
-const loadAdds = () =>
-{
-   console.log('loadAdds');
-}
+const loadAdds = () => {
+	console.log("loadAdds");
+};
 
 //camabio 1
 //camabio 2
 //camabio 3
+
+//cambios videos 1
+//cambios videos 2
+//cambios videos 2
